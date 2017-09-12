a83d0f9022b7cafb7544ee26cae899934a83a992
cs.gomez <cs.gomez@ISIS2603S4-0035.sis.virtual.uniandes.edu.co>
2017-09-11 20:15:08 -0500

    Entities, Persistencias y Tests Exitosos de los recursos: Artista, Cancion y CarroCompras.

---
+++ tiendaVinilos/tiendaVinilos-back/src/main/java/co/edu/uniandes/csw/tiendaVinilos/entities/ArtistaEntity.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.tiendaVinilos.entities;
+
+import javax.persistence.Entity;
+
+/**
+ *
+ * @author cs.gomez
+ */
+@Entity
+public class ArtistaEntity extends DefaultEntity{
+    
+}
