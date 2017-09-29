83895a3eb1592e401812f3cf089424a9bc34c425
jc.ruiz <jc.ruiz@ISIS2603S4-0040.sis.virtual.uniandes.edu.co>
2017-09-28 20:06:41 -0500

    Intentando arreglar la vida

--- tiendaVinilos-web/src/main/java/co/edu/uniandes/csw/tiendaVinilos/resources/ProveedorResource.java
+++ tiendaVinilos-web/src/main/java/co/edu/uniandes/csw/tiendaVinilos/resources/ProveedorResource.java
@@ -72,7 +72,7 @@ public class ProveedorResource {
         entity.setId(id);
         ProveedorEntity oldEnt = logic.getProveedor(id);
         if(oldEnt == null)
-            throw new WebApplicationException("El feed back con el id " + id + " no existe ", 404); 
+            throw new WebApplicationException("El proveedor con el id " + id + " no existe ", 404); 
         ProveedorEntity ent = logic.updateProveedor(entity);
         return (new ProveedorDetailDTO(ent));
     }
