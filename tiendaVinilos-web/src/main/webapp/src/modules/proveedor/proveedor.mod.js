b2118a43c4a9338f7ee3c0435b4602d724386ec6
jc.ruiz <jc.ruiz@ISIS2603S4-0040.sis.virtual.uniandes.edu.co>
2017-10-21 23:25:23 -0500

    Modulo implementado

---
+++ tiendaVinilos-web/src/main/webapp/src/modules/proveedor/proveedor.mod.js
@@ -0,0 +1,32 @@
+(function (ng) {
+var mod = ng.module("proveedorModules", []);
+    mod.constant("proveedorContext", "api/proveedores");
+    mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+            var basePath = 'src/modules/proveedor/';
+            $urlRouterProvider.otherwise("/proveedorList");
+            $stateProvider.state('proveedorList', {
+                url: '/proveedores',
+                views: {
+                    'mainView': {
+                        controller: 'proveedorCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'proveedor.list.html'
+                    }
+                }
+            }).state('proveedorSee', {
+                url: '/proveedores/:proveedorId',
+                param:{
+                    proveedorId: null
+                },
+                views: {
+                    'mainView': {
+                        controller: 'proveedorCtrl',
+                        controllerAs: 'ctrl',
+                        templateUrl: basePath + 'proveedor.see.html'
+                    }
+                }
+            });
+        }]);
+
+})(window.angular);
+
