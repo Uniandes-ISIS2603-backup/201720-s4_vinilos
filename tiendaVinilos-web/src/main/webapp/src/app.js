8e92095f72fc13b16ca51b577fa6c45eb3b8c0e5
SIS\jp.monsalvo <jp.monsalvo@uniandes.edu.co>
2017-10-30 11:35:34 -0500

    Vinilo add, delete, list

--- tiendaVinilos-web/src/main/webapp/src/app.js
+++ tiendaVinilos-web/src/main/webapp/src/app.js
@@ -4,7 +4,8 @@
         'ui.router',
        
         // Internal modules dependencies       
-        'proveedorModules'
+        'proveedorModules',
+        'viniloModule'
 
     ]);
     // Resuelve problemas de las promesas
