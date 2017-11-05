b3e41f0f0bae2f217c59a3a7b8dfb9c6a81ed289
JULIAN DAVID ARENAS SICACHA <jd.arenas@sis.virtual.uniandes.edu.co>
2017-11-04 20:08:55 -0500

    tarjeta with post

--- tiendaVinilos-web/src/main/webapp/src/modules/tarjeta2/tarjeta.create.ctrl.js
+++ tiendaVinilos-web/src/main/webapp/src/modules/tarjeta2/tarjeta.create.ctrl.js
@@ -1,11 +1,12 @@
 (function (ng) {
 
     var mod = ng.module("tarjetaModules");
-    mod.controller("tarjeraCreateCtrl", ['$scope', '$state', '$stateParams', '$http', 'tarjetaContext', function ($scope, $state, $stateParams, $http, context) {
+    mod.controller("tarjetaCreateCtrl", ['$scope', '$state', '$stateParams', '$http', 'tarjetaContext', function ($scope, $state, $stateParams, $http, context) {
 
              this.editTarjeta = function(){
                 return $http.post(context , {
-
+                    numero:$scope.tarjetaNumber,
+                    nombrePropietario:$scope.tarjetaOwner
                     }).then(function () {
                                 alert($scope.tarjetaNumber)
                                 alert($scope.tarjetaOwner)
